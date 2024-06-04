eb09516428c1d2a7bd3b088c2c16f3481676f3d9
Stef Robbe <stef.robbe@protonmail.com>
2024-06-04 20:00:37 +0200

    fix: attempt another method for COPR

--- build.sh
+++ build.sh
@@ -12,10 +12,9 @@ RELEASE="$(rpm -E %fedora)"
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/40/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# Use COPR repo's with rpm-ostree: https://github.com/boredsquirrel/COPR-command
-# TODO: https://discussion.fedoraproject.org/t/need-help-using-copr-in-fedora-silverblue/74522/2
-wget https://raw.githubusercontent.com/trytomakeyouprivate/COPR-OSTree/main/copr -P /tmp && chmod +x /tmp/copr
-echo "y" | /tmp/copr enable varlad/zellij
+# Use COPR zellij repo with rpm-ostree: https://discussion.fedoraproject.org/t/how-to-use-copr-on-fedora-atomic/106067/3
+sudo curl --output-dir "/etc/yum.repos.d/" --remote-name https://copr.fedorainfracloud.org/coprs/varlad/zellij/repo/fedora-40/varlad-zellij-fedora-40.repo
+sudo rpm-ostree refresh-md --force
 
 rpm-ostree install fish helix zellij zoxide eza fd-find ripgrep bat
 
