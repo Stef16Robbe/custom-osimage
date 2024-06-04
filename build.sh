520666e36d72db02fd7b78f9cf971e6a4dff2a3f
Stef Robbe <stef.robbe@protonmail.com>
2024-06-04 19:42:37 +0200

    ci: add packages to build script

--- build.sh
+++ build.sh
@@ -10,14 +10,14 @@ RELEASE="$(rpm -E %fedora)"
 # Packages can be installed from any enabled yum repo on the image.
 # RPMfusion repos are available by default in ublue main images
 # List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/40/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
-rpm-ostree install screen
+# Use COPR repo's with rpm-ostree: https://github.com/boredsquirrel/COPR-command
+# TODO: https://discussion.fedoraproject.org/t/need-help-using-copr-in-fedora-silverblue/74522/2
+wget https://raw.githubusercontent.com/trytomakeyouprivate/COPR-OSTree/main/copr -P /tmp && chmod +x /tmp/copr
+/tmp/copr enable varlad/zellij
 
-# this would install a package from rpmfusion
-# rpm-ostree install vlc
+rpm-ostree install fish helix zellij zoxide eza fd-find ripgrep bat
 
 #### Example for enabling a System Unit File
-
 systemctl enable podman.socket
