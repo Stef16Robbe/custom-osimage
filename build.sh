081348ec26275cbfc29009a403af6b8e6766b3b9
Stef <stef.robbe@protonmail.com>
2024-06-13 19:04:24 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,12 +11,10 @@ RELEASE="$(rpm -E %fedora)"
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/40/x86_64/repoview/index.html&protocol=https&redirect=1
 
-rpm-ostree upgrade
-
 # Use COPR zellij repo with rpm-ostree: https://discussion.fedoraproject.org/t/how-to-use-copr-on-fedora-atomic/106067/3
 curl --output-dir "/etc/yum.repos.d/" --remote-name https://copr.fedorainfracloud.org/coprs/varlad/zellij/repo/fedora-40/varlad-zellij-fedora-40.repo
 
-rpm-ostree install fish helix zellij zoxide eza fd-find ripgrep bat
+rpm-ostree install --force-replacefiles fish helix zellij zoxide eza fd-find ripgrep bat
 
 #### Example for enabling a System Unit File
 systemctl enable podman.socket
